ea8f75fbd7812b2068b846ae64215fb004aa6fa7
JustPil <88640664+JustPil@users.noreply.github.com>
2022-02-14 21:36:46 -0800

    Minor style updates

--- src/FCF/SubstitutionCipher.java
+++ src/FCF/SubstitutionCipher.java
@@ -1,10 +1,9 @@
-package FCF;
+package SC;
 
 import java.io.IOException;
 import java.io.RandomAccessFile;
 
-public class SubstitutionCipher
-{
+public class SubstitutionCipher {
     private int shift;
     private StringBuilder ciphertext;
     private char[] charArray;
@@ -22,16 +21,14 @@ public class SubstitutionCipher
      * @param s The integer to add to each character of the message.
      * @throws IOException An I/O exception has occurred.
      */
-    public SubstitutionCipher(String plain, int s) throws IOException
-    {
+    public SubstitutionCipher(String plain, int s) throws IOException {
         shift = s;
         rafPlain = new RandomAccessFile(PLAINFILE, "rw");
         long seeker = 0;
         charArray = plain.toCharArray();
-        for(int i = 0; i < charArray.length; i++)
-        {
+        for(char c : charArray) {
             rafPlain.seek(seeker);
-            rafPlain.writeChar(charArray[i]);
+            rafPlain.writeChar(c);
             seeker += 2;
         }
         rafPlain.close();
@@ -43,12 +40,10 @@ public class SubstitutionCipher
      * written to a ciphertext binary file with the shift integer added, changing the ASCII value of each character.
      * @throws IOException An I/O exception has occurred.
      */
-    public void createCipher() throws IOException
-    {
+    public void createCipher() throws IOException {
         rafPlain = new RandomAccessFile(PLAINFILE, "r");
         rafCipher = new RandomAccessFile(CIPHERFILE, "rw");
-        for(int i = 0; i < charArray.length; i++)
-        {
+        for(int i = 0; i < charArray.length; i++) {
             char ch = rafPlain.readChar();
             ch += shift;
             rafCipher.writeChar(ch);
@@ -62,11 +57,9 @@ public class SubstitutionCipher
      *readPlain Reads the plaintext message from the plaintext binary file.
      * @throws IOException An I/O exception has occurred.
      */
-    public void readPlain() throws IOException
-    {
+    public void readPlain() throws IOException {
         rafPlain = new RandomAccessFile(PLAINFILE, "r");
-        for(int i = 0; i < charArray.length; i++)
-        {
+        for(int i = 0; i < charArray.length; i++) {
             char ch = rafPlain.readChar();
             System.out.print(ch);
         }
@@ -77,11 +70,11 @@ public class SubstitutionCipher
      *readCipher Reads the ciphertext message from the ciphertext binary file.
      * @throws IOException An I/O exception has occurred.
      */
-    public void readCipher() throws IOException
-    {
+    public void readCipher() throws IOException {
         rafCipher = new RandomAccessFile(CIPHERFILE, "r");
-        for(int i = 0; i < charArray.length; i++)
+        for(int i = 0; i < charArray.length; i++) {
             System.out.print(rafCipher.readChar());
+        }
         System.out.println();
         rafCipher.close();
     }
@@ -92,18 +85,15 @@ public class SubstitutionCipher
      * @return The decrypted message in a StringBuilder.
      * @throws IOException An I/O exception has occurred.
      */
-    public StringBuilder decrypt() throws IOException
-    {
-        if(!cipherSet)
-        {
+    public StringBuilder decrypt() throws IOException {
+        if(!cipherSet) {
             ciphertext = new StringBuilder();
             ciphertext.append("Error: There is no cipher set to decrypt.");
             return ciphertext;
         }
         ciphertext = new StringBuilder();
         rafCipher = new RandomAccessFile(CIPHERFILE, "r");
-        for(int i = 0; i < charArray.length; i++)
-        {
+        for(int i = 0; i < charArray.length; i++) {
             char ch = rafCipher.readChar();
             ch -= shift;
             ciphertext.append(ch);
